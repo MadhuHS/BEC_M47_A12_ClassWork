d3f337ed86e963a551e300b39efdb71cfb34dbc7
Madhu <Madhu@192.168.1.11>
2021-07-19 13:28:57 +0530

    Customer entity and Services updated

---
+++ Banking_App/src/com/jspiders/bankingapp/entities/Customer.java
@@ -0,0 +1,15 @@
+package com.jspiders.bankingapp.entities;
+
+public class Customer
+{
+	int accountID;
+	String customerName;
+	String email;
+	String address;
+    String pancard;
+    
+    public static void CustomerDetails()
+    {
+    	
+    }
+}
