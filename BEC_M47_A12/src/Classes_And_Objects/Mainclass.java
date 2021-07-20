bcc235ab2b9e24e2b1dec2682bec26e3ae1c0084
Madhu <Madhu@192.168.1.11>
2021-07-20 12:35:01 +0530

    added click() to Demo class

--- BEC_M47_A12/src/Classes_And_Objects/Mainclass.java
+++ BEC_M47_A12/src/Classes_And_Objects/Mainclass.java
@@ -3,25 +3,49 @@ package Classes_And_Objects;
 class Demo
 {
 	static double z1 = 151.2;//static-Data member
+	double k1 = 151.2;//Non-Static-Data member
 	
 	public static void display()//static-Function member
 	{
 		System.out.println("this is display() of Demo class");
+	}
+	
+	public void show()//Non-Static-Function member
+	{
+		System.out.println("this is show() of Demo class");
+	}
+	
+	public void click()//Non-Static-Function member
+	{
+		System.out.println("this is click() of Demo class");
 	}
 }
 
 public class Mainclass 
 {
 	static int v1 = 172;//static-Data member
+	double v2 = 2.67;//Non-Static-Data member
+	
 	
 	public static void view()//static-Function member
 	{
 		System.out.println("this is view() of Main class");
 	}
 	
+	
+	public void count()//Non-Static-Function member
+	{
+		System.out.println("this is count() of Main class");
+		System.out.println(v2);//Non-Static - same class
+		
+		new Demo().show();//Non-Static - different class
+	}
+	
+	
 	public static void main(String[] args)//static-Function member
 	{
 	   System.out.println("Program starts...");
+	   
 	   
 	   view();//static - same class
 	   System.out.println(v1);//static - same class
@@ -31,6 +55,12 @@ public class Mainclass
 	   
 	   Sample.test();//static - different class
 	   System.out.println(Sample.m1);//static - different class
+	   
+	   new Mainclass().count();//Non-Static - same class
+	   System.out.println(new Mainclass().v2);//Non-Static - same class
+	   
+	   new Demo().show();//Non-Static - different class
+	   System.out.println(new Demo().k1);//Non-Static- different class
 	   
 	   System.out.println("Program ends..");
 	}
